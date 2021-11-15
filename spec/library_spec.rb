516aa09f3ea9baf6a01846880ba134311cfbfa84
giacoletti <giacoletti92@gmail.com>
2021-11-15 10:11:51 +0100

    Adding availability and return date attributes to Library class, and unit tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,12 +1,23 @@
 require "./lib/library.rb"
 
 RSpec.describe Library do
+  describe "collection of books" do
+    it "is expected to have a collection of books as an Array" do
+      expect(subject.books).to be_instance_of Array
+    end
 
-  it "is expected to have a collection of books as an Array" do
-    expect(subject.books).to be_instance_of Array
+    it "is expected to hold 5 items/books" do
+      expect(subject.books.size).to eq 5
+    end
   end
 
-  it "is expected to hold 5 items/books" do
-    expect(subject.books.size).to eq 5
+  describe "a book" do
+    it "is expected to have an availability status" do
+      expect(subject.books.first.keys).to include(:available)
+    end
+
+    it "is expected to have a return date" do
+      expect(subject.books.first.keys).to include(:return_date)
+    end
   end
 end
